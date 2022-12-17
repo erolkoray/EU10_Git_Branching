dedb007f4f503615f0f4ede88d77aaa6e1e814a9
Koray <koray_4404@hotmail.com>
2022-12-17 14:38:09 +0000

    added negative testing TC001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -8,7 +8,9 @@ public class TC001 {
 
         //testing some stuff
 
-        // more testing for 2nd commit (without pushing at this stage)
+        // more testing
+
+        //negative testing
 
     }
 }
